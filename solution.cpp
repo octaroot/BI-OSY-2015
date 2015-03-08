264ce298aaa1efbf06be32e31e54ef0778c3305e
Martin Černáč <me@martincernac.cz>
2015-03-08 11:45:23 +0100

    Fix the cache generation algo

--- solution.cpp
+++ solution.cpp
@@ -49,44 +49,63 @@ void MapAnalyzer(int threads, const TCostProblem *(*costFunc)(void), const TCrim
 
 bool FindByCost(int **values, int size, int maxCost, TRect *res) {
 
+    const int actualSize = size + 1;
+
     //cache int table (SAT table)
-    int **cache = new int *[size];
+    int **cache = new int *[actualSize];
 
     //building the cache
-    //we fill the 0,y and x,0 with initial values to make the main O(n^2) do less conditionals
+    //we fill the 0,y and x,0 with initial values to make the main O(n^2) do no conditionals
 
     //the [0,0]
-    cache[0] = new int[size];
-    cache[0][0] = values[0][0];
+    cache[0] = new int[actualSize];
+    cache[0][0] = 0;
 
     //the [x,0] and [0,y]
-    for (int i = 1; i < size; ++i) {
-        cache[i] = new int[size];
-
-        cache[0][i] = values[0][i] + values[0][i - 1];
-        cache[i][0] = values[i][0] + values[i - 1][0];
+    for (int i = 1; i < actualSize; ++i) {
+        cache[i] = new int[actualSize];
+        cache[i][0] = 0;
+        cache[0][i] = 0;
     }
 
     //the rest. no conditionals, yay!
-    for (int i = 1; i < size; ++i) {
-        for (int j = 1; j < size; ++j) {
-            cache[i][j] = values[i][j] + values[i - 1][j] + values[i][j - 1] - values[i - 1][j - 1];
+    for (int i = 1; i < actualSize; ++i) {
+        for (int j = 1; j < actualSize; ++j) {
+            cache[i][j] = values[i - 1][j - 1] + cache[i - 1][j] + cache[i][j - 1] - cache[i - 1][j - 1];
         }
     }
 
-    int maxSuitableArea = 0;
-    TRect result;
+    int maxSuitableArea = 0, totalCost, area;
 
-    //todo O(n^4)
+    //O(n^4) search
+    for (int x = 1; x < actualSize; ++x) {
+        for (int y = 1; y < actualSize; ++y) {
+            for (int h = 1; h < x; ++h) {
+                for (int w = 1; w < y; ++w) {
 
+                    totalCost = values[x][y] - values[h - 1][w] - values[h][w - 1] + values[h - 1][w - 1];
 
-    for (int i = 0; i < size; ++i) {
-        delete [] cache[i];
+                    if (totalCost < maxCost && (area = (y - h) * (x - w)) > maxSuitableArea) {
+                        res->m_H = y - h;
+                        res->m_W = x - w;
+                        res->m_X = x - 1;
+                        res->m_Y = y - 1;
+
+                        maxSuitableArea = area;
+                    }
+                }
+            }
+        }
     }
 
-    delete [] cache;
+    //freeing of cache allocated resources
+    for (int i = 0; i < actualSize; ++i) {
+        delete[] cache[i];
+    }
 
-    return false;
+    delete[] cache;
+
+    return maxSuitableArea != 0;
 }
 
 bool FindByCrime(double **values, int size, double maxCrime, TRect *res) {
@@ -102,16 +121,17 @@ bool FindByCrime(double **values, int size, double maxCrime, TRect *res) {
         }
     }
 
-    int maxSuitableArea = 0;
-    TRect result;
+    /*int maxSuitableArea = 0;
+    TRect result;*/
 
-    // todo O(n^3)
+    //TODO O(n^3)
 
+    //freeing of cache allocated resources
     for (int i = 0; i < size; ++i) {
-        delete [] cache[i];
+        delete[] cache[i];
     }
 
-    delete [] cache;
+    delete[] cache;
 
     return false;
 }
